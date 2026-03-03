95e206721bbaa1a947a1545ef899cf30299df512
AjayKumar1523 <143533253+AjayKumar1523@users.noreply.github.com>
2026-03-03 08:50:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const modal = document.getElementById("modal");
 const openBtn = document.getElementById("openModal");
+const modal = document.getElementById("modal");
 const closeBtn = document.querySelector(".close-modal");
 
-openBtn.onclick = function () {
-    modal.style.display = "block";   // MUST BE block
+openBtn.onclick = () => {
+  modal.style.display = "block";
 };
 
-closeBtn.onclick = function () {
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+};
+
+modal.onclick = (event) => {
+  if (event.target === modal) {
     modal.style.display = "none";
-};
-
-modal.onclick = function (e) {
-    if (e.target === modal) {
-        modal.style.display = "none";
-    }
+  }
 };

3b89ed42666ff359b5a2efef69ce1493c7fee1d5
AjayKumar1523 <143533253+AjayKumar1523@users.noreply.github.com>
2026-03-03 08:44:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+const modal = document.getElementById("modal");
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.querySelector(".close-modal");
+
+openBtn.onclick = function () {
+    modal.style.display = "block";   // MUST BE block
+};
+
+closeBtn.onclick = function () {
+    modal.style.display = "none";
+};
+
+modal.onclick = function (e) {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+};
